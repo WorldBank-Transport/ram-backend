933b615fa1230d34e007be1282bebfd55cb83b4c
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2016-02-18 15:25:09 -0500

    path.exists has been changed to fs.exists

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ http.createServer(function(request, response) {
 
   var uri = url.parse(request.url).pathname
     , filename = path.join(process.cwd(), uri);
-  
-  path.exists(filename, function(exists) {
+
+  fs.exists(filename, function(exists) {
     if(!exists) {
       response.writeHead(404, {"Content-Type": "text/plain"});
       response.write("404 Not Found\n");
@@ -20,7 +20,7 @@ http.createServer(function(request, response) {
     if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
     fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
+      if(err) {
         response.writeHead(500, {"Content-Type": "text/plain"});
         response.write(err + "\n");
         response.end();
