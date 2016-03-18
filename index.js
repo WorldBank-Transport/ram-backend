cf3793a30e582fb41e6a808cde004cebc875aad2
stvno <gitlab@minst.net>
2016-03-18 17:29:25 -0400

    robust childproces

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ var http = require("http"),
     port = process.argv[2] || 8888,
     fork = require('child_process').fork;
 
-var timematrix = fork('./scripts/node/timematrix.js');
+function timematrix() {
+   var tm = fork('./scripts/node/timematrix.js');
+
+    tm.on('disconnect', function () {
+      console.warn('timematrix disconnected!');
+    });
+
+    tm.on('close', function () {
+      console.warn('timematrix crashed!');
+      timematrix()
+    });
+}
+
+timematrix();
 
 http.createServer(function(request, response) {
 
