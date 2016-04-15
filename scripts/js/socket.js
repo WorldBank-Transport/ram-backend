3be4689b8af5836c8b240ce063de70a936587f09
Steven M. Ottens <gitlab@minst.net>
2016-04-15 12:15:53 -0400

    port issues with amazon

--- scripts/js/socket.js
+++ scripts/js/socket.js
@@ -4,7 +4,7 @@ d3.json('../data/user.json',function(d){
 })
 var socket;
 function Authenticate(user,pass) {
-  var sockethost = window.location.protocol +'//'+ window.location.host.split(':')[0] + ':50000'
+  var sockethost = window.location.protocol +'//'+ window.location.host.split(':')[0];
   socket = io(sockethost);
   var uploader = new SocketIOFileUpload(socket);
   uploader.listenOnInput(document.getElementById("siofu_input"));
@@ -35,7 +35,7 @@ function Authenticate(user,pass) {
           d3.select('#csvlist')
           .selectAll("tr")
           .data(data.csvs)
-          .enter()qq
+          .enter()
           .insert('tr',":first-child")
           .html(createCsvList)
       }
--- scripts/js/step3.js
+++ scripts/js/step3.js
@@ -5,7 +5,7 @@ d3.json('../data/user.json',function(d){
 })
 var socket;
 function Authenticate(user,pass) {
-  var sockethost = window.location.protocol +'//'+ window.location.host.split(':')[0] + ':50000'
+  var sockethost = window.location.protocol +'//'+ window.location.host.split(':')[0];
   socket = io(sockethost);
   socket.on('connect', function(){
     socket.emit('authentication', {username: user, password: pass});
