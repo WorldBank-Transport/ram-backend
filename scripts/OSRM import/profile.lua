4f6315ebc36781f05fef6386b5b82587df977b60
Steven M. Ottens <gitlab@minst.net>
2016-04-28 14:00:43 -0400

    more flexible profile

--- scripts/OSRM import/profile.lua
+++ scripts/OSRM import/profile.lua
@@ -51,10 +51,7 @@ speed_profile = {
 upgrade_speeds = {
   ["County"]  = 40,
   ["Township"] = 40,
-  ["Rural"] = 30,
-  ["Expressway"] = 120,
-  ["National"] = 80,
-  ["Provincial"] = 60
+  ["Rural"] = 30
 }
 
 -- surface/trackype/smoothness
@@ -350,11 +347,11 @@ function way_function (way, result)
   -- upgrade roads 
   local county = way:get_value_by_key("CountryUpgrade")
   local township = way:get_value_by_key("TownshipUpgrade")
-  if county and "1" == county then
+  if county and "1" == county and upgrade_speeds[highway] then
     result.forward_speed = math.max(upgrade_speeds[highway], result.forward_speed)
     result.backward_speed = math.max(upgrade_speeds[highway], result.backward_speed)
   end
-  if township and "1" == township then
+  if township and "1" == township and upgrade_speeds[highway] then
     result.forward_speed = math.max(upgrade_speeds[highway], result.forward_speed)
     result.backward_speed = math.max(upgrade_speeds[highway], result.backward_speed)
   end
