be3cb7d1e83b62da70daea7a0995796036772a4e
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2016-10-04 18:45:12 -0400

    ln -> cp

--- test/prepare-test.sh
+++ test/prepare-test.sh
@@ -3,8 +3,7 @@ set -x
 
 echo "Preparing fixtures data for test"
 
-ln -s $(pwd)/test/fixtures/data $(pwd)/web/data
-ls
+cp -r $(pwd)/test/fixtures/data $(pwd)/web/data
 cd $(pwd)/web/data
 ls
 osmosis --rbf JM.pbf --wx JM.osm
