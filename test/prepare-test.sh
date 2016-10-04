9473569a6a79a7f580e93602a025d39b25e962fe
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2016-10-04 18:28:50 -0400

    paths

--- test/prepare-test.sh
+++ test/prepare-test.sh
@@ -2,7 +2,9 @@
 set -x
 
 echo "Preparing fixtures data for test"
-ln -s test/fixtures/data web/data
-cd web/data
+pwd
+ls
+ln -s ./test/fixtures/data ./web/data
+cd ./web/data
 
 osmosis --rbf JM.pbf --wx JM.osm
