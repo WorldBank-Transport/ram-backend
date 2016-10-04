eebbfa19814ad82180a6473b5284967cab8436e5
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2016-10-04 18:38:04 -0400

    ln stores relatives paths

--- test/prepare-test.sh
+++ test/prepare-test.sh
@@ -2,9 +2,8 @@
 set -x
 
 echo "Preparing fixtures data for test"
-pwd
-ls
-ln -s ./test/fixtures/data ./web/data
-cd ./web/data
+
+ln -s $(pwd)/test/fixtures/data $(pwd)/web/data
+cd web/data
 
 osmosis --rbf JM.pbf --wx JM.osm
