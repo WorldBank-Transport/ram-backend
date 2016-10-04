1270a6308f84cb53771a8cc16fb3c0417d5adbdd
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2016-10-04 19:34:11 -0400

    pull latests osmosis

--- test/prepare-test.sh
+++ test/prepare-test.sh
@@ -5,5 +5,14 @@ echo "Preparing fixtures data for test"
 
 cp -r $(pwd)/test/fixtures/data $(pwd)/web/data
 cd $(pwd)/web/data
-ls
-osmosis --rbf JM.pbf --wx JM.osm
+
+wget http://bretth.dev.openstreetmap.org/osmosis-build/osmosis-latest.tgz
+mkdir osmosis
+mv osmosis-latest.tgz osmosis
+cd osmosis
+tar xvfz osmosis-latest.tgz
+rm osmosis-latest.tgz
+chmod a+x bin/osmosis
+cd ../
+
+osmosis/bin/osmosis --rbf JM.pbf --wx JM.osm
