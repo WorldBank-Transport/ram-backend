6a3cd55ff6571a50a6ecd59fbdc8acfe6e242991
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2016-10-05 19:35:20 -0400

    zombies needs this it seems.
    so it can load it as a module

--- index.js
+++ index.js
@@ -436,3 +436,11 @@ function createTimeMatrix(data) {
     }
   });
 }
+
+// app is a callback function or an express application
+module.exports = app;
+if (!module.parent) {
+  http.listen(process.env.PORT, function(){
+    console.log("Server listening on port " + app.get('port'));
+  });
+}
