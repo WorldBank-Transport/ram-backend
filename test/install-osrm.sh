b52b4aee78934a97d584cf4e9f7e6c04b7ee4822
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2016-10-12 10:37:07 +0900

    Fix OSRM build

--- test/install-osrm.sh
+++ test/install-osrm.sh
@@ -4,7 +4,7 @@ set -x
 echo "Installing osrm-backend dependencies..."
 sudo apt-get install build-essential git cmake pkg-config \
 libbz2-dev libstxxl-dev libstxxl1 libxml2-dev \
-libzip-dev libboost-all-dev lua5.1 liblua5.1-0-dev libluabind-dev libtbb-dev
+libzip-dev libboost-all-dev lua5.2 liblua5.2-0-dev libluabind-dev libtbb-dev
 
 echo "Installing osrm-backend..."
 git clone https://github.com/Project-OSRM/osrm-backend.git
