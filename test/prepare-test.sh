b61ce4e5e9794b0770105a93fccaa91cfbd5bdfb
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2016-10-12 17:36:26 +0900

    TODOs from pure OSM
    Should it be here in fixtures or in main

--- test/prepare-test.sh
+++ test/prepare-test.sh
@@ -4,6 +4,7 @@ set -x
 echo "Preparing fixtures data for test"
 cd $(pwd)/web/data
 
+echo "Installing osmosis"
 wget http://bretth.dev.openstreetmap.org/osmosis-build/osmosis-latest.tgz
 mkdir osmosis
 mv osmosis-latest.tgz osmosis
@@ -14,7 +15,14 @@ chmod a+x bin/osmosis
 osrm_dir=$(pwd)/bin
 cd ../project_1463767649122/baseline
 
+echo "Creating OSM & OSRM files"
 rm *.os*m *.log
 $osrm_dir/osmosis --rbf JM.pbf --wx JM.osm
 osrm-extract JM.osm
 osrm-contract JM.osrm
+
+echo "Extracting villages and POIs from OSM"
+#TODO
+
+echo "Adding fake population"
+#TODO
