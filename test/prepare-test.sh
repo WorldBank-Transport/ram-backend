be7c4a7b41c32c696e08f86d4f8c5ae931207849
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2016-11-18 13:42:05 -0500

    WIP dirs

--- test/prepare-test.sh
+++ test/prepare-test.sh
@@ -3,6 +3,7 @@ set -x
 
 echo "Preparing fixtures data for test"
 cd $(pwd)/web/data
+rra_dir=$(pwd)
 
 echo "Installing osmosis"
 wget http://bretth.dev.openstreetmap.org/osmosis-build/osmosis-latest.tgz
@@ -18,8 +19,13 @@ cd ../project_1463767649122/baseline
 echo "Creating OSM & OSRM files"
 rm *.os*m *.log
 $osrm_dir/osmosis --rbf JM.pbf --wx JM.osm
-osrm-extract JM.osm
-osrm-contract JM.osrm
+
+#link lua and lib files
+cp -r $osrm_dir/profiles/lib $osrm_dir/build/
+cp "$rra_dir/scripts/OSRM\ import/profile.lua" ~/code/osrm-backend/build/
+cd $rra_dir/web/data/project_1463767649122/baseline
+$osrm_dir/build/osrm-extract JM.osm
+$osrm_dir/build/osrm-contract JM.osrm
 
 echo "Extracting villages and POIs from OSM"
 #TODO
