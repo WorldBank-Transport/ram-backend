9be42234cdff129e6f0a608bd340e3edc32b61a7
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2016-11-30 15:13:19 -0500

    locally create osrm

--- test/prepare-test.sh
+++ test/prepare-test.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 set -x
 
-echo "Preparing fixtures data for test"
-cd $(pwd)/web/data
-rra_dir=$(pwd)
+python_ver=$(python -c 'import sys; print(sys.version_info[0])')
+if [[ $python_ver -ne 2 ]] ; then
+    echo "node-gyp breaks when not on Python 2. Exiting..."
+    exit 1
+fi
 
-echo "Installing osmosis"
-wget http://bretth.dev.openstreetmap.org/osmosis-build/osmosis-latest.tgz
-mkdir osmosis
-mv osmosis-latest.tgz osmosis
-cd osmosis
-tar xvfz osmosis-latest.tgz
-rm osmosis-latest.tgz
-chmod a+x bin/osmosis
-osrm_dir=$(pwd)/bin
-cd ../project_1463767649122/baseline
+
+echo "Preparing fixtures data for test"
+ln -s test/fixtures/data web/data
+
+rra_dir=$(pwd)
+data_dir=$rra_dir/web/data/project_1463767649122/baseline
+osrm_dir=$rra_dir/node_modules/osrm/lib/binding/
+
 
 echo "Creating OSM & OSRM files"
-rm *.os*m *.log
-$osrm_dir/osmosis --rbf JM.pbf --wx JM.osm
+unzip $data_dir/JM.zip -d $data_dir/
 
 #link lua and lib files
+mkdir -p $osrm_dir/build/
 cp -r $osrm_dir/profiles/lib $osrm_dir/build/
-cp "$rra_dir/scripts/OSRM\ import/profile.lua" ~/code/osrm-backend/build/
-cd $rra_dir/web/data/project_1463767649122/baseline
-$osrm_dir/build/osrm-extract JM.osm
-$osrm_dir/build/osrm-contract JM.osrm
+cp "$rra_dir/scripts/OSRM import/profile.lua" $osrm_dir/
+cd $data_dir
+$osrm_dir/osrm-extract JM.osm
+$osrm_dir/osrm-contract JM.osrm
 
 echo "Extracting villages and POIs from OSM"
 #TODO
