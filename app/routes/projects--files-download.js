3a64bc4c30737f7b2bdbc2d9842098c5b26342b7
Daniel da Silva <daniel@developmentseed.org>
2017-03-03 15:40:56 +0000

    Add file download endpoint

---
+++ app/routes/projects--files-download.js
@@ -0,0 +1,56 @@
+'use strict';
+import Joi from 'joi';
+import Boom from 'boom';
+
+import db from '../db/';
+import { getFile } from '../s3/utils';
+import { FileNotFoundError } from '../utils/errors';
+
+module.exports = [
+  {
+    path: '/projects/{projId}/files/{fileId}/download',
+    method: 'GET',
+    config: {
+      validate: {
+        params: {
+          projId: Joi.number(),
+          fileId: Joi.number()
+        }
+      }
+    },
+    handler: (request, reply) => {
+      db('projects_files')
+        .select('*')
+        .where('id', request.params.fileId)
+        .where('project_id', request.params.projId)
+        .then(res => {
+          if (!res.length) throw new FileNotFoundError();
+          return res[0];
+        })
+        .then(file => {
+          return getFile(file.path)
+            .then(dataStream => {
+              let mime;
+              switch (file.type) {
+                case 'profile':
+                  mime = 'text/x-lua';
+                  break;
+                case 'villages':
+                case 'admin-bounds':
+                  mime = 'application/json';
+                  break;
+              }
+
+              reply(dataStream)
+                .type(mime)
+                .header('Content-Disposition', `attachment; filename=${file.name}`);
+            });
+        })
+        .catch(FileNotFoundError, e => reply(Boom.notFound(e.message)))
+        .catch(err => {
+          console.log('err', err);
+          reply(Boom.badImplementation(err));
+        });
+    }
+  }
+];
---
+++ app/routes/scenario--files-download.js
@@ -0,0 +1,60 @@
+'use strict';
+import Joi from 'joi';
+import Boom from 'boom';
+
+import db from '../db/';
+import { getFile } from '../s3/utils';
+import { FileNotFoundError } from '../utils/errors';
+
+module.exports = [
+  {
+    path: '/projects/{projId}/scenarios/{scId}/files/{fileId}/download',
+    method: 'GET',
+    config: {
+      validate: {
+        params: {
+          projId: Joi.number(),
+          scId: Joi.number(),
+          fileId: Joi.number()
+        }
+      }
+    },
+    handler: (request, reply) => {
+      db('scenarios_files')
+        .select('*')
+        .where('id', request.params.fileId)
+        .where('project_id', request.params.projId)
+        .where('scenario_id', request.params.scId)
+        .then(res => {
+          if (!res.length) throw new FileNotFoundError();
+          return res[0];
+        })
+        .then(file => {
+          return getFile(file.path)
+            .then(dataStream => {
+              let mime;
+              switch (file.type) {
+                case 'poi':
+                  mime = 'application/json';
+                  break;
+                case 'road-network':
+                  mime = 'application/xml';
+                  break;
+              }
+
+              reply(dataStream)
+                .type(mime)
+                .header('Content-Disposition', `attachment; filename=${file.name}`);
+            });
+        })
+        .catch(FileNotFoundError, e => reply(Boom.notFound(e.message)))
+        .catch(err => {
+          console.log('err', err);
+          if (err.code === 'NoSuchKey') {
+            return reply(Boom.notFound('File not found in storage bucket.'));
+          }
+          reply(Boom.badImplementation(err));
+        });
+    }
+  }
+];
