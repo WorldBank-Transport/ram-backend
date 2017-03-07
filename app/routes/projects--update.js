36ae110ca54f9f3e3c221dcde073aa0dbd72bd05
Daniel da Silva <daniel@developmentseed.org>
2017-03-07 14:23:23 +0000

    Extract finish project setup to different file

--- app/routes/projects--update.js
+++ app/routes/projects--update.js
@@ -1,12 +1,10 @@
 'use strict';
 import Joi from 'joi';
 import Boom from 'boom';
-import Promise from 'bluebird';
 
 import db from '../db/';
 
 import { ProjectNotFoundError, DataConflictError } from '../utils/errors';
-import { getProject } from './projects--get';
 
 module.exports = [
   {
@@ -53,70 +51,6 @@ module.exports = [
         console.log('err', err);
         reply(Boom.badImplementation(err));
       });
-    }
-  },
-  {
-    path: '/projects/{projId}/finish-setup',
-    method: 'POST',
-    config: {
-      validate: {
-        params: {
-          projId: Joi.number()
-        },
-        payload: {
-          scenarioName: Joi.string().required(),
-          scenarioDescription: Joi.string()
-        }
-      }
-    },
-    handler: (request, reply) => {
-      getProject(request.params.projId)
-        .then(project => {
-          if (project.status !== 'pending') {
-            throw new DataConflictError('Project setup already completed');
-          }
-          if (!project.readyToEndSetup) {
-            throw new DataConflictError('Project preconditions to finish setup not met');
-          }
-          return project;
-        })
-        .then(project => {
-          // Main scenario id.
-          return db.select('id')
-            .from('scenarios')
-            .where('project_id', project.id)
-            .orderBy('created_at')
-            .limit(1)
-            .then(res => db.transaction(function (trx) {
-              let scenarioId = res[0].id;
-
-              let {scenarioName, scenarioDescription} = request.payload;
-
-              return Promise.all([
-                trx('projects')
-                  .update({
-                    updated_at: (new Date()),
-                    status: 'active'
-                  })
-                  .where('id', project.id),
-                trx('scenarios')
-                  .update({
-                    name: scenarioName,
-                    description: typeof scenarioDescription === 'undefined' ? '' : scenarioDescription,
-                    updated_at: (new Date()),
-                    status: 'active'
-                  })
-                  .where('id', scenarioId)
-              ]);
-            }));
-        })
-        .then(() => reply({statusCode: 200, message: 'Project setup finished'}))
-        .catch(ProjectNotFoundError, e => reply(Boom.notFound(e.message)))
-        .catch(DataConflictError, e => reply(Boom.conflict(e.message)))
-        .catch(err => {
-          console.log('err', err);
-          reply(Boom.badImplementation(err));
-        });
     }
   }
 ];
