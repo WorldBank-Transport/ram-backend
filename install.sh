63836dfdc11da24dbb369659b976ade6e7191f7f
Olaf Veerman <olaf.veerman@gmail.com>
2017-04-13 11:54:38 -0400

    Dockerize the tests on Travis To have more control over the environment, tests on Travis will run in a Docker container

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-apt-get update
-apt-get install -y curl build-essential
+echo 'Installing the dependencies'
+apt-get -qq update
+apt-get -qq install -y curl build-essential
 
 # install nodejs
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
-apt-get install -y nodejs
+apt-get -qq install -y nodejs
 
 # install Hyper
 curl -sL https://hyper-install.s3.amazonaws.com/hyper-linux-x86_64.tar.gz | tar xzf -
@@ -13,4 +14,4 @@ chmod +x hyper
 mv ./hyper /usr/local/bin
 
 # install dependencies for ogr2osm
-apt-get install -y python-gdal python-lxml
+apt-get -qq install -y python-gdal python-lxml
