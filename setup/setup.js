d5885e5c92294b258d03977c12e1023596589dbb
Daniel da Silva <daniel@developmentseed.org>
2017-05-02 16:59:32 +0100

    Include fixture version of osm-p2p-db

--- setup/setup.js
+++ setup/setup.js
@@ -1,5 +1,7 @@
 'use strict';
 import fs from 'fs-extra';
+import path from 'path';
+import Promise from 'bluebird';
 
 import { setupStructure as setupDb } from '../app/db/structure';
 import { setupStructure as setupS3 } from '../app/s3/structure';
@@ -8,7 +10,20 @@ import config from '../app/config';
 
 const rmOsmP2PDbs = () => {
   console.log('Removing osm-p2p dbs:', config.osmP2PDir);
-  return fs.remove(config.osmP2PDir);
+  return Promise.promisify(fs.remove)(config.osmP2PDir);
+};
+
+const addOsmP2PData = () => {
+  console.log('Adding osm-p2p dbs:', config.osmP2PDir);
+  const src = path.resolve(__dirname, '../test/utils/data-sergipe/osm-p2p-db');
+  const copy = Promise.promisify(fs.copy);
+
+  return Promise.all([
+    copy(src, path.resolve(config.osmP2PDir, `p${1100}s${1100}`)),
+    copy(src, path.resolve(config.osmP2PDir, `p${1200}s${1200}`)),
+    copy(src, path.resolve(config.osmP2PDir, `p${1200}s${1201}`)),
+    copy(src, path.resolve(config.osmP2PDir, `p${2000}s${2000}`))
+  ]);
 };
 
 const arg = (a) => process.argv.indexOf(a) !== -1;
@@ -19,6 +34,7 @@ if (arg('--data')) {
   fns.push(() => setupDb());
   fns.push(() => setupS3());
   fns.push(() => addData());
+  fns.push(() => addOsmP2PData());
 } else {
   if (arg('--db')) {
     fns.push(() => rmOsmP2PDbs());
