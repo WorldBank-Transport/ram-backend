978eb5db8639bfb93980744eb7be3703633c8175
Olaf Veerman <olaf.veerman@gmail.com>
2017-05-11 10:11:07 -0400

    Add offline config file

---
+++ app/config/offline.js
@@ -0,0 +1,26 @@
+module.exports = {
+  environment: 'offline',
+  debug: false,
+  connection: {
+    host: '0.0.0.0',
+    port: 4000
+  },
+  db: 'postgresql://rra:rra@172.99.99.10:5432/rra',
+  osmP2PDir: `${__dirname}/../../osm-p2p-dbs`,
+  storage: {
+    host: '172.99.99.15',
+    port: 9000,
+    engine: 'minio',
+    accessKey: 'minio',
+    secretKey: 'miniostorageengine',
+    bucket: 'rra',
+    region: 'us-east-1'
+  },
+  analysisProcess: {
+    service: 'docker',
+    container: 'wbtransport/rra-analysis:latest-stable',
+    db: 'postgresql://rra:rra@172.99.99.10:5432/rra',
+    storageHost: '172.99.99.15',
+    storagePort: 9000
+  }
+};
