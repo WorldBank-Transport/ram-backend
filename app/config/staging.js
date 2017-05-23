942b0864be1a7221358c9c70c27847b80e696305
Olaf Veerman <olaf.veerman@gmail.com>
2017-05-23 11:22:23 -0400

    Switch storage to S3 on staging - switch storage to S3 - remove the Minio container

--- app/config/staging.js
+++ app/config/staging.js
@@ -8,12 +8,12 @@ module.exports = {
   db: 'postgresql://rra:rra@rra-postgis:5432/rra',
   osmP2PDir: `${__dirname}/../../osm-p2p-dbs`,
   storage: {
-    host: '34.207.194.24',
-    port: 9000,
-    engine: 'minio',
-    accessKey: 'minio',
-    secretKey: 'miniostorageengine',
-    bucket: 'rra',
+    host: null,
+    port: null,
+    engine: 's3',
+    accessKey: 'AKIAIPEH472XR6VB5U3A',
+    secretKey: null,
+    bucket: 'rra-staging-storage',
     region: 'us-east-1'
   },
   analysisProcess: {
