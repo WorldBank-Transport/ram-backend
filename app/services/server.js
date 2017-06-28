cf892e03a0840584de059a4a5857e918a5ca5dc7
Marc Farra <marcfarra@gmail.com>
2017-06-28 10:51:28 -0400

    Move auth registration to plugins.js

--- app/services/server.js
+++ app/services/server.js
@@ -1,5 +1,4 @@
 import Hapi from 'hapi';
-import jwksRsa from 'jwks-rsa';
 import config from '../config';
 
 module.exports = function (options, callback) {
@@ -21,36 +20,9 @@ module.exports = function (options, callback) {
 
   server.hapi.connection(server.options.connection);
 
-  server.hapi.register(require('hapi-auth-jwt2'), err => {
+  // Bootstrap Hapi Server Plugins, passes the server object to the plugins.
+  require('./plugins')(server.hapi, function (err) {
     if (err) callback(err);
-
-    if (config.auth && config.auth.strategy === 'jwt') {
-      server.hapi.auth.strategy('jwt', 'jwt', true, {
-        complete: true,
-        key: jwksRsa.hapiJwt2Key({
-          cache: true,
-          rateLimit: true,
-          jwksRequestsPerMinute: 5,
-          jwksUri: `${config.auth.issuer}.well-known/jwks.json`
-        }),
-        verifyOptions: {
-          audience: config.auth.audience,
-          issuer: config.auth.issuer,
-          algorithms: ['RS256']
-        },
-        validateFunc: (decoded, request, callback) => {
-          if (decoded && decoded.sub) {
-            return callback(null, true);
-          }
-          return callback(null, false);
-        }
-      });
-    }
-
-    // Bootstrap Hapi Server Plugins, passes the server object to the plugins.
-    require('./plugins')(server.hapi, function (err) {
-      if (err) throw err;
-    });
 
     server.start = function (cb) {
       server.hapi.log(['info'], 'Database connected');
