d3c5760a5099224e648d7bef8093117e3b7c7b76
Olaf Veerman <olaf.veerman@gmail.com>
2018-05-30 17:05:38 -0400

    Tag release on master with version from package.json

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -3,11 +3,20 @@ set -e
 
 # Setting correct variables based on the environment we're deploying to
 if [[ $TRAVIS_BRANCH == ${DEVELOP_BRANCH} ]]; then
-  LATEST_TAG=latest-dev
-  ECS_CLUSTER=${AWS_ECS_CLUSTER_STAGING}
+  VERSION=latest-dev
 elif [[ $TRAVIS_BRANCH == ${STABLE_BRANCH} ]]; then
-  LATEST_TAG=latest-stable
-  ECS_CLUSTER=${AWS_ECS_CLUSTER}
+  # Grab version from package.json and prepend with v (v0.5.0)
+  VERSION=v$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+  # Attempt to add a git tag based on version in package.json. If
+  # the tag already exists, git will fail and stop the build.
+  if ! git tag ${VERSION} master
+  then
+    echo >&2 "Failed to tag a new release, skipping build. Did you update the version in package.json?"
+    exit 1
+  else
+    # Push tag to Github
+    git push origin ${VERSION}
+  fi
 fi
 
 echo "Building source image"
@@ -19,6 +28,6 @@ echo "Pushing image to Docker Hub:$TRAVIS_COMMIT"
 docker tag $DOCKER_SRC_IMAGE $DOCKER_REPOSITORY:$TRAVIS_COMMIT
 docker push $DOCKER_REPOSITORY:$TRAVIS_COMMIT
 
-echo "Also pushing as :$LATEST_TAG"
-docker tag $DOCKER_SRC_IMAGE $DOCKER_REPOSITORY:$LATEST_TAG
-docker push $DOCKER_REPOSITORY:$LATEST_TAG
+echo "Also pushing as :$VERSION"
+docker tag $DOCKER_SRC_IMAGE $DOCKER_REPOSITORY:$VERSION
+docker push $DOCKER_REPOSITORY:$VERSION
