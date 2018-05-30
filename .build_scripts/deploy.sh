c811c6539b8ef5ee1f1340286bc6e378e0371b5f
Olaf Veerman <olaf.veerman@gmail.com>
2018-05-30 15:23:42 -0400

    Remove deploy related config Automatic deployment is not part of this repo anymore.

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -22,20 +22,3 @@ docker push $DOCKER_REPOSITORY:$TRAVIS_COMMIT
 echo "Also pushing as :$LATEST_TAG"
 docker tag $DOCKER_SRC_IMAGE $DOCKER_REPOSITORY:$LATEST_TAG
 docker push $DOCKER_REPOSITORY:$LATEST_TAG
-
-echo "Installing ecs-cli"
-sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
-sudo chmod +x /usr/local/bin/ecs-cli
-
-echo "Building ECS task"
-npm install yamljs
-node .build_scripts/insert-env.js
-
-echo "Configuring ECS client"
-ecs-cli configure --region $AWS_REGION --access-key $AWS_ACCESS_KEY_ID --secret-key $AWS_SECRET_ACCESS_KEY --cluster $ECS_CLUSTER
-
-echo "Stopping current ECS task"
-ecs-cli compose --verbose --project-name ruralroads-backend --file ecs-task-definition-generated.yml service stop
-
-echo "Deploying to ECS"
-ecs-cli compose --verbose --project-name ruralroads-backend --file ecs-task-definition-generated.yml service up
