e220e8e5fd870dc6244086251b1ef42a65fb4fb2
Daniel da Silva <daniel@developmentseed.org>
2018-12-18 16:28:10 +0000

    Promisify gen results

--- app/routes/scenarios--gen-results.js
+++ app/routes/scenarios--gen-results.js
@@ -28,82 +28,78 @@ module.exports = [
         }
       }
     },
-    handler: (request, reply) => {
-      const { projId, scId } = request.params;
+    handler: async (request, reply) => {
+      try {
+        const { projId, scId } = request.params;
+        const opCheck = new Operation(db);
 
-      let op = new Operation(db);
-      op.loadByData('generate-analysis', projId, scId)
-        .then(op => {
-          if (op.isStarted()) {
-            throw new DataConflictError('Result generation already running');
-          }
-        }, err => {
+        try {
+          // Try to load the operation to see if it exists.
+          await opCheck.loadByData('generate-analysis', projId, scId);
+        } catch (err) {
           // In this case if the operation doesn't exist is not a problem.
-          if (err.message.match(/not exist/)) { return; }
-          throw err;
-        })
+          if (!err.message.match(/not exist/)) throw err;
+        }
+
+        if (opCheck.isStarted()) {
+          throw new DataConflictError('Result generation already running');
+        }
+
         // Valid project ?
         // Projects already setup ?
-        .then(() => getProject(projId))
-        .then(project => {
-          if (project.status !== 'active') {
-            throw new DataConflictError('Project setup not completed');
-          }
-          return project;
-        })
-        // Valid scenario ?
-        .then(() => db.select('*')
+        const project = await getProject(projId);
+        if (project.status !== 'active') {
+          throw new DataConflictError('Project setup not completed');
+        }
+
+        const scenario = await db.select('*')
           .from('scenarios')
           .where('id', scId)
           .where('project_id', projId)
-          .then(scenarios => {
-            if (!scenarios.length) throw new ScenarioNotFoundError();
-            return scenarios[0];
-          })
-          // Admin areas selected ?
-          .then(scenario => db('scenarios_settings')
-            .select('value')
-            .where('key', 'admin_areas')
-            .where('scenario_id', scId)
-            .first()
-            .then(setting => {
-              if (setting.value === '[]') {
-                throw new DataConflictError('No admin areas selected');
-              }
-            })
-        )
+          .first();
+        if (!scenario) throw new ScenarioNotFoundError();
+
+        // Admin areas selected ?
+        const aaSetting = await db('scenarios_settings')
+          .select('value')
+          .where('key', 'admin_areas')
+          .where('scenario_id', scId)
+          .first();
+        if (aaSetting.value === '[]') {
+          throw new DataConflictError('No admin areas selected');
+        }
+
         // Good to go.
         // Delete all existing results. (s3 and database)
-        .then(() => db('scenarios_files')
+        const files = await db('scenarios_files')
           .select('*')
           .where('scenario_id', scId)
           .where('project_id', projId)
-          .whereIn('type', ['results-csv', 'results-json', 'results-geojson'])
-          .then(files => {
-            let tasks = files.map(f => removeFile(f.path));
-            let ids = files.map(f => f.id);
-            return Promise.all(tasks)
-              .then(() => db('scenarios_files')
-                .whereIn('id', ids)
-                .del()
-              )
-              .then(() => db('results')
-                .where('project_id', projId)
-                .where('scenario_id', scId)
-                .del()
-              );
-          }))
-        )
-        // Create an operation.
-        .then(() => {
-          op = new Operation(db);
-          return op.start('generate-analysis', projId, scId);
-        })
-        .then(op => op.log('start', {message: 'Analysis generation started'}))
-        // Start generation.
-        .then(op => generateResults(projId, scId, op))
-        .then(() => reply({statusCode: 200, message: 'Result generation started'}))
-        .catch(err => reply(getBoomResponseForError(err)));
+          .whereIn('type', ['results-csv', 'results-json', 'results-geojson']);
+
+        const rmFilesTasks = files.map(f => removeFile(f.path));
+        const ids = files.map(f => f.id);
+        await Promise.all([
+          ...rmFilesTasks,
+          db('scenarios_files')
+            .whereIn('id', ids)
+            .del(),
+          db('results')
+            .where('project_id', projId)
+            .where('scenario_id', scId)
+            .del()
+        ]);
+
+        // Generate the results.
+        // Will start an operation and handle error with it.
+        // The promise resolves in the background.
+        // This is intentional.
+        generateResults(projId, scId);
+
+        return reply({statusCode: 200, message: 'Result generation started'});
+      } catch (error) {
+        return reply(getBoomResponseForError(error));
+      }
     }
   },
   {
@@ -117,86 +113,108 @@ module.exports = [
         }
       }
     },
-    handler: (request, reply) => {
-      const { projId, scId } = request.params;
+    handler: async (request, reply) => {
+      try {
+        const { projId, scId } = request.params;
+        const op = new Operation(db);
 
-      let op = new Operation(db);
-      op.loadByData('generate-analysis', projId, scId)
-        .then(op => {
-          if (!op.isStarted()) {
-            throw new DataConflictError('Result generation not running');
-          }
-        }, err => {
-          // In this case if the operation doesn't exist is not a problem.
+        try {
+          // Try to load the operation to see if it exists.
+          await op.loadByData('generate-analysis', projId, scId);
+        } catch (err) {
+          // Can's stop the operation if it doesn't exist.
           if (err.message.match(/not exist/)) {
             throw new DataConflictError('Result generation not running');
           }
-          throw err;
-        })
+        }
+
+        if (!op.isStarted()) {
+          throw new DataConflictError('Result generation not running');
+        }
+
         // Send kill signal to generation process.
-        .then(() => killAnalysisProcess(projId, scId))
+        await killAnalysisProcess(projId, scId);
+
         // Abort operation.
-        .then(() => op.finish('error', {error: 'Operation aborted'}))
-        .then(() => reply({statusCode: 200, message: 'Result generation aborted'}))
-        .catch(err => reply(getBoomResponseForError(err)));
+        await op.finish('error', {error: 'Operation aborted'});
+
+        return reply({statusCode: 200, message: 'Result generation aborted'});
+      } catch (error) {
+        return reply(getBoomResponseForError(error));
+      }
     }
   }
 ];
 
-function generateResults (projId, scId, op) {
+function setImmediatePromise (cb) {
+  return new Promise((resolve, reject) => {
+    setImmediate(() => {
+      cb().then(resolve).catch(reject);
+    });
+  });
+}
+
+async function generateResults (projId, scId) {
   // In test mode we don't want to start the generation.
   // It will be tested in the appropriate place.
   if (process.env.DS_ENV === 'test') { return; }
 
-  let opId = op.getId();
+  const identifier = `p${projId} s${scId}`;
+  const op = new Operation(db);
 
-  let identifier = `p${projId} s${scId}`;
-  if (!runningProcesses[identifier]) runningProcesses[identifier] = {};
+  try {
+    // Start the operation.
+    await op.start('generate-analysis', projId, scId);
+    await op.log('start', {message: 'Analysis generation started'});
+    const opId = op.getId();
 
-  // Check if we need to export the road network.
-  db('scenarios_settings')
-    .select('value')
-    .where('scenario_id', scId)
-    .whereIn('key', ['res_gen_at', 'rn_active_editing', 'rn_updated_at'])
-    .orderBy('key')
-    .then(scSettings => {
-      const activeEditing = scSettings[1].value;
-      let genAt = scSettings[0].value;
-      let rnUpdatedAt = scSettings[2].value;
+    if (!runningProcesses[identifier]) runningProcesses[identifier] = {};
 
-      let needExport = false;
-      if (activeEditing === 'true') {
-        genAt = genAt === 0 ? genAt : (new Date(genAt)).getTime();
-        rnUpdatedAt = rnUpdatedAt === 0 ? rnUpdatedAt : (new Date(rnUpdatedAt)).getTime();
+    // Check if we need to export the road network.
+    let [genAt, activeEditing, rnUpdatedAt] = await db('scenarios_settings')
+      .select('value')
+      .where('scenario_id', scId)
+      .whereIn('key', ['res_gen_at', 'rn_active_editing', 'rn_updated_at'])
+      .orderBy('key')
+      .then(res => res.map(r => r.value));
 
-        needExport = rnUpdatedAt > genAt;
-        if (!needExport) {
-          console.log(identifier, 'Road network was not modified');
-        }
-      } else {
-        console.log(identifier, 'Road network editing not enabled');
+    let needExport = false;
+    if (activeEditing === 'true') {
+      genAt = genAt === 0 ? genAt : (new Date(genAt)).getTime();
+      rnUpdatedAt = rnUpdatedAt === 0 ? rnUpdatedAt : (new Date(rnUpdatedAt)).getTime();
+      needExport = rnUpdatedAt > genAt;
+
+      if (!needExport) {
+        console.log(identifier, 'Road network was not modified');
+      }
+    } else {
+      console.log(identifier, 'Road network editing not enabled');
+    }
+
+    await setImmediatePromise(async () => {
+      if (needExport) {
+        // Close the database on this thread process before exporting the
+        // road network on a new thread process. (using a service runner)
+        await closeDatabase(projId, scId);
+        await updateRN(projId, scId, opId);
+        await generateTiles(projId, scId, op);
       }
 
-      setImmediate(() => {
-        let executor = Promise.resolve();
-
-        if (needExport) {
-          executor = executor
-            // Close the database on this process before exporting the road network.
-            .then(() => closeDatabase(projId, scId))
-            .then(() => updateRN(projId, scId, opId))
-            .then(() => generateTiles(projId, scId, op));
-        }
-
-        executor.then(() => spawnAnalysisProcess(projId, scId, opId))
-          .catch(err => {
-            console.log(identifier, 'generateResults error was handled:', err);
-          });
-      });
+      await spawnAnalysisProcess(projId, scId, opId);
     });
+
+  // Catching errors.
+  } catch (error) {
+    // The operation may not have finished if the error took place outside
+    // the promise, or if the error was due to a wrong db connection.
+    if (!op.isCompleted()) {
+      await op.finish('error', {error: error.message || error});
+    }
+    console.log(identifier, 'generateResults error was handled:', error);
+  }
 }
 
-function updateRN (projId, scId, opId, cb) {
+function updateRN (projId, scId, opId) {
   return new Promise((resolve, reject) => {
     let identifier = `p${projId} s${scId}`;
     console.log(identifier, 'updateRN');
@@ -210,16 +228,7 @@ function updateRN (projId, scId, opId, cb) {
 
       if (err) {
         console.log(identifier, 'updateRN ended in error and was captured');
-        // The operation may not have finished if the error took place outside
-        // the promise, or if the error was due to a wrong db connection.
-        let op = new Operation(db);
-        op.loadById(opId)
-          .then(op => {
-            if (!op.isCompleted()) {
-              return op.finish('error', {error: err.message});
-            }
-          })
-          .then(() => reject(err), () => reject(err));
+        reject(err);
       } else {
         resolve();
       }
@@ -228,40 +237,28 @@ function updateRN (projId, scId, opId, cb) {
   });
 }
 
-function generateTiles (projId, scId, op) {
-  let identifier = `p${projId} s${scId}`;
+async function generateTiles (projId, scId, op) {
+  const identifier = `p${projId} s${scId}`;
   console.log(identifier, 'generating vector tiles');
 
-  let executor = db('scenarios_files')
+  const rnFile = await db('scenarios_files')
     .select('*')
     .where('scenario_id', scId)
     .where('type', 'road-network')
-    .first()
-    .then(file => {
-      // createRoadNetworkVT returns an objects with a promise and a kill switch
-      let service = createRoadNetworkVT(projId, scId, op, file.path);
-      runningProcesses[identifier].genVT = service;
+    .first();
 
-      return service.promise;
-    })
-    .then(() => {
-      runningProcesses[identifier].genVT = null;
-    })
-    .catch(err => {
-      let executor = Promise.resolve();
-      if (!op.isCompleted()) {
-        executor = executor
-          .then(() => op.finish('error', {error: err.message}));
-      }
+  // createRoadNetworkVT returns an objects with a promise and a kill switch
+  const service = createRoadNetworkVT(projId, scId, op, rnFile.path);
+  runningProcesses[identifier].genVT = service;
 
-      // Rethrow to stop;
-      return executor.then(() => { throw err; });
-    });
+  // Wait for the service to finish.
+  await service.promise;
 
-  return executor;
+  // Cleanup.
+  runningProcesses[identifier].genVT = null;
 }
 
-function spawnAnalysisProcess (projId, scId, opId) {
+async function spawnAnalysisProcess (projId, scId, opId) {
   // Update image before starting.
   function pullImage () {
     return new Promise((resolve, reject) => {
@@ -361,16 +358,7 @@ function spawnAnalysisProcess (projId, scId, opId) {
         delete runningProcesses[identifier];
 
         if (code !== 0) {
-          // The operation may not have finished if the error took place outside
-          // the promise, or if the error was due to a wrong db connection.
-          let op = new Operation(db);
-          return op.loadById(opId)
-            .then(op => {
-              if (!op.isCompleted()) {
-                return op.finish('error', {error: error});
-              }
-            })
-            .then(() => reject(error), () => reject(error));
+          return reject(new Error(error));
         }
 
         return resolve();
@@ -378,15 +366,15 @@ function spawnAnalysisProcess (projId, scId, opId) {
     });
   }
 
-  return pullImage()
-    .then(() => runProcess());
+  await pullImage();
+  await runProcess();
 }
 
 function killAnalysisProcess (projId, scId) {
   if (process.env.DS_ENV === 'test') { return Promise.resolve(); }
 
   return new Promise((resolve, reject) => {
-    let identifier = `p${projId} s${scId}`;
+    const identifier = `p${projId} s${scId}`;
     // Since the processes run sequentially check by order which we need
     // to kill.
     if (runningProcesses[identifier].updateRN) {
